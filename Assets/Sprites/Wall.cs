bffddfce2dcec2fb9b238858400e112495871461
Kamisama16 <newworldskami97@gmail.com>
2024-02-23 22:25:04 -0500

    Wall file and camera file update

---
+++ Assets/Sprites/Wall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    [HideInInspector] public float centerX, centerY, left, top, right, bottom;
+    BoxCollider2D boxCollider;
+
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        centerX = transform.position.x + boxCollider.bounds.extents.x;
+        centerY = transform.position.y - boxCollider.bounds.extents.y;
+        left = transform.position.x;
+        top = transform.position.y;
+        right = transform.position.x + boxCollider.bounds.size.x;
+        bottom = transform.position.y - boxCollider.bounds.size.y;
+    }
+}
