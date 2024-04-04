80096b02fe3e2c26d8c887da2adfc886d6eaadba
Kamisama16 <newworldskami97@gmail.com>
2024-04-04 05:45:44 -0400

    EVERYTINGWORKSdecently

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+    public AudioSource audioSource;
     public void OpenDoor()
     {
         // Move the door to the right to "open" it
         transform.position += Vector3.right * 10;
+        audioSource.Play();
     }
 }
